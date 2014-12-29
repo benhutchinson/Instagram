4cd2b3824308274a9e9d870f348e7a14707452a8
benhutchinson <ben.hutchinson.personal@gmail.com>
2014-12-29 15:11:06 +0000

    Building Posts ActiveRecord Model

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,11 +1,24 @@
 require 'rails_helper'
 
 feature 'posts' do
-  context 'no posts have been added' do
+  context 'have not yet been added' do
     scenario 'should display a prompt to post a picture' do
       visit '/posts'
       expect(page).to have_content 'No posts'
       expect(page).to have_link 'Post'
     end
   end
+
+  context 'have been added' do
+    before do
+      Post.create(username: 'ben', comment: 'busted, i am the real david bailey')
+    end
+
+    scenario 'show posts' do
+      visit '/posts'
+      expect(page).to have_content('david bailey')
+      expect(page).not_to have_content('No posts')
+    end
+  end
+
 end

78633b43d9de602f0b78a0c8bd21bf4d22eefa08
benhutchinson <ben.hutchinson.personal@gmail.com>
2014-12-29 16:23:31 +0000

    Added Post Icon To Nav Bar

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'posts' do
+feature 'posts: ' do
   context 'have not yet been added' do
     scenario 'should display a prompt to post a picture' do
       visit '/posts'
@@ -18,6 +18,17 @@ feature 'posts' do
       visit '/posts'
       expect(page).to have_content('david bailey')
       expect(page).not_to have_content('No posts')
+    end
+  end
+
+  context 'adding new posts' do
+    scenario 'prompts a user to fill out a form, and then displays the new post' do
+      visit '/posts'
+      click_link 'Post'
+      fill_in 'comment', with: 'david bailey'
+      click_button 'Post'
+      expect(page).to have_content 'david bailey'
+      expect(current_path).to eq '/restaurants'
     end
   end
 

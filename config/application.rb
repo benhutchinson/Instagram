cd8ded187ec484b18466afd54d9f5bccea0b8445
benhutchinson <ben.hutchinson.personal@gmail.com>
2015-01-01 17:30:34 +0000

    Heroku Test

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,6 @@ module Instagram
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.compile = true
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end

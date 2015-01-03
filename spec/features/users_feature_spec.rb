6b217fbe6ae53bdf09f07c43cf32ff4291820c6f
benhutchinson <ben.hutchinson.personal@gmail.com>
2015-01-03 12:31:16 +0000

    User redirection/log-in tests

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -1,37 +1,56 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# context "user not signed in and on the homepage" do
-#   it "should see a 'sign in' link and a 'sign up' link" do
-#     visit('/')
-#     expect(page).to have_link('Sign in')
-#     expect(page).to have_link('Sign up')
-#   end
+context "user not signed in" do
 
-#   it "should not see 'sign out' link" do
-#     visit('/')
-#     expect(page).not_to have_link('Sign out')
-#   end
-# end
+  it "should see a log-in form and a 'sign up' link on the homepage" do
+    visit('/')
+    assert has_field?('username')
+    assert has_field?('password')
+    assert has_button?('log-in')
+    expect(page).to have_link('sign-up')
+  end
 
-# context "user signed in on the homepage" do
+  it "should not see 'sign out' link" do
+    visit('/')
+    expect(page).not_to have_link('Sign out')
+  end
 
-#   before do
-#     visit('/')
-#     click_link('Sign up')
-#     fill_in('Email', with: 'test@example.com')
-#     fill_in('Password', with: 'testtest')
-#     fill_in('Password confirmation', with: 'testtest')
-#     click_button('Sign up')
-#   end
+  it "should be redirected to the homepage if trying to view main content" do
+    visit('/posts')
+    assert has_field?('username')
+    assert has_field?('password')
+    assert has_button?('log-in')
+    expect(page).to have_link('sign-up')
+  end
 
-#   it "should see 'sign out' link" do
-#     visit('/')
-#     expect(page).to have_link('Sign out')
-#   end
+  it "should be redirected to the homepage if trying to post" do
+    visit('/posts/new')
+    expect(current_path).to eql "/users/sign_in"
+  end
 
-#   it "should not see a 'sign in' link and a 'sign up' link" do
-#     visit('/')
-#     expect(page).not_to have_link('Sign in')
-#     expect(page).not_to have_link('Sign up')
-#   end
-# end
+end
+
+context "user signed-up and signed-in" do
+
+  include Warden::Test::Helpers
+  Warden.test_mode!
+
+  before do
+    @current_user = User.create!(:username => 'user', :email => 'test@test.com', :password => 'password')
+    login_as(@current_user, :scope => :user)
+    visit '/'
+  end
+
+  it "should go straight to content and see 'no posts' if there is no content" do
+    expect(page).to have_content 'No posts'
+    expect(page).to have_link 'Post'
+  end
+
+  it "should not see a 'sign in' link and a 'sign up' link" do
+    assert has_no_field?('username')
+    assert has_no_field?('password')
+    assert has_no_button?('log-in')
+    expect(page).not_to have_link('sign-up')
+  end
+
+end
